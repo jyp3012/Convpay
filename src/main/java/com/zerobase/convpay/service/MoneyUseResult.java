c427f469d4a3f1aca698a2a6ac9ff5426ad9a71a
pjy3012 <ppp0868@naver.com>
2022-06-28 20:47:26 +0900

    편결이 결제 시스템 연습!

---
+++ src/main/java/com/zerobase/convpay/service/MoneyUseResult.java
@@ -0,0 +1,6 @@
+package com.zerobase.convpay.service;
+
+public enum MoneyUseResult {
+    USE_SUCCESS,
+    USE_FAIL
+}
---
+++ src/test/java/com/zerobase/convpay/service/MoneyAdapterTest.java
@@ -0,0 +1,35 @@
+package com.zerobase.convpay.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyAdapterTest {
+    MoneyAdapter moneyAdapter = new MoneyAdapter();
+
+
+    @Test
+    void money_use_fail() {
+        //given
+        Integer payAmount = 1000_001;
+
+        //when
+        MoneyUseResult moneyUseResult = moneyAdapter.use(payAmount);
+
+        //then
+        assertEquals(moneyUseResult.USE_FAIL, moneyUseResult);
+      }
+
+    @Test
+    void money_use_success() {
+        //given
+        Integer payAmount = 1000_000;
+
+        //when
+        MoneyUseResult moneyUseResult = moneyAdapter.use(payAmount);
+
+        //then
+        assertEquals(moneyUseResult.USE_SUCCESS, moneyUseResult);
+    }
+
+}
